b4a4f5ad02d00501e3f50f571e40e5c13c6991bb
denizyuksel <d.yuksel@ug.bilkent.edu.tr>
2018-07-01 19:09:25 +0200

    Quiz Activity Layout draft is done.

---
+++ app/src/main/java/com/lglab/merino/lgxeducontroller/activities/QuizActivity.java
@@ -0,0 +1,15 @@
+package com.lglab.merino.lgxeducontroller.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lglab.merino.lgxeducontroller.R;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
