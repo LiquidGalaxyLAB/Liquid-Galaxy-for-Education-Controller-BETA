b465f47e88916155007381b63d26493aa204e709
denizyuksel <d.yuksel@ug.bilkent.edu.tr>
2018-07-01 20:44:09 +0200

    Quiz activity and Play activity layout draft done.

--- app/src/main/java/com/lglab/merino/lgxeducontroller/activities/PlayActivity.java
+++ app/src/main/java/com/lglab/merino/lgxeducontroller/activities/PlayActivity.java
@@ -1,8 +1,12 @@
 package com.lglab.merino.lgxeducontroller.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.lglab.merino.lgxeducontroller.R;
 
@@ -15,11 +19,24 @@ public class PlayActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle(R.string.play);
+        startViews();
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    public void startViews(){
+
+        ImageView homeImage = (ImageView)findViewById(R.id.homeIcon);
+        homeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PlayActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
