55533b7f1100f48cd0fa2ad77745e5f8104dadeb
Ivan Colomer <ivancolomer.1@gmail.com>
2018-07-12 11:54:27 +0200

    Nice smoothy indicator ;)

--- app/src/main/java/com/lglab/merino/lgxeducontroller/fragments/QuestionFragment.java
+++ app/src/main/java/com/lglab/merino/lgxeducontroller/fragments/QuestionFragment.java
@@ -1,4 +1,35 @@
 package com.lglab.merino.lgxeducontroller.fragments;
 
-public class QuestionFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.lglab.merino.lgxeducontroller.R;
+
+public class QuestionFragment extends Fragment{
+    private int bgRes;
+    private ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bgRes = getArguments().getInt("data");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_question, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        imageView = getView().findViewById(R.id.image);
+        imageView.setBackgroundResource(bgRes);
+    }
 }
