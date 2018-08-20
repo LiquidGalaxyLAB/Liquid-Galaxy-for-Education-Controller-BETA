1ebf138c50eb1601b805f72a323d5a3c2183d410
Aleks Genov <samuilov.aleks@gmail.com>
2018-08-20 13:32:29 +0200

    Copied Activities and code from Ivan J. and put some PopUp in CreateQuestion

---
+++ app/src/main/java/com/lglab/merino/lgxeducontroller/activities/CreateItemActivity_Copy.java
@@ -0,0 +1,22 @@
+package com.lglab.merino.lgxeducontroller.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lglab.merino.lgxeducontroller.R;
+import com.lglab.merino.lgxeducontroller.legacy.CreateItemFragment;
+
+
+public class CreateItemActivity_Copy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_item);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, CreateItemFragment_Copy.newInstance())
+                    .commit();
+        }
+    }
+}
